ec1f7f231b399b5e151fe7c31411215679f1030c
Sahil Sharma <sahil@Sahils-MacBook-Pro.local>
2016-07-07 13:48:58 +0530

    Wiring Signup including password encryption

---
+++ server/controllers/auth.js
@@ -0,0 +1,38 @@
+const User = require('../models/user');
+
+exports.signup = function (req, res, next) {
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //check if email and password are provided in the body
+  if(!email || !password) {
+    res.status(422).send({error : 'You must provide email and password'});
+  }
+
+  User.findOne({email}, function (err, existingUser) {
+    if(err) { return next(err); }
+
+    //check if a user with an email does exist
+    if(existingUser) {
+      return res.status(422).send({error : 'Email already in use'});
+    }
+
+    //if user with email does not exist, create and save the user
+    const newUser = new User({
+      email,
+      password
+    });
+
+    // respond with the new user detail
+    newUser.save(function (err, user) {
+      if(err) { return next(err); }
+
+      if(user) {
+        res.json(user);
+      }
+    })
+
+  })
+
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
+const Authentication = require('../controllers/auth');
+
 module.exports = function(express, opts) {
-  var controllers = require('../controllers')(opts);
+  const controllers = require('../controllers')(opts);
   var router = express.Router();
+
+  router.post('/signup', Authentication.signup);
 
   router.get('/contacts', controllers.index);
   router.post('/contacts', controllers.addContact);
