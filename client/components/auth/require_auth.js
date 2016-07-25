f11619757c0a1d7dca5850cfb8ecc0a4fdbbbd1e
Sahil Sharma <sahil@Sahils-MacBook-Pro.local>
2016-07-25 18:52:13 +0530

    Adding Higher order component

---
+++ client/components/auth/require_auth.js
@@ -0,0 +1,34 @@
+//This is a higher order component to make sure the user has already logged in
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
