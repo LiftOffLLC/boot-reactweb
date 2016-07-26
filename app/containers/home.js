26988787deed445ba6cd6762c10e6648d20a9beb
chetanb <chetankumar01@gmail.com>
2016-07-26 13:39:36 +0530

    configured live reload on dev server

--- app/containers/home.js
+++ app/containers/home.js
@@ -5,7 +5,11 @@ import React, { Component, PropTypes } from 'react';
 class Home extends Component{
   render(){
     return(
-      <h1>Home page</h1>
+      <div>
+        <h1>Home page</h1>
+        <h2>Hello There</h2>
+        <h2>Hello There</h2>
+      </div>
     );
   }
 }
