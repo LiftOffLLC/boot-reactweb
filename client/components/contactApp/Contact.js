ea7b802504cfe7e6e4c53c7a0077e4e21ce11ab3
Sahil Sharma <sahil@Sahils-MacBook-Pro.local>
2016-08-05 13:41:15 +0530

    Implement Drag and drop

--- client/components/contactApp/Contact.js
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react';
-
-const Contact = (contact) => (
-  <li className="js-contact">
-    {contact.name}
-  </li>
-);
-
-export default Contact;
--- client/components/contactApp/ContactList.js
+++ client/components/contactApp/ContactList.js
@@ -2,26 +2,36 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/contactApp';
 
-import Contact from './contact';
 import AddContact from './AddContact';
+
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+
+const SortableItem = SortableElement(({value}) => <li>{value.name}</li>);
+
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <ul className="contact-list">
+      {items.map((value, index) =>
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      )}
+    </ul>
+  );
+});
 
 class ContactList extends React.Component{
   componentWillMount(){
     this.props.fetchContacts();
   }
-
+  onSortEnd = ({oldIndex, newIndex}) => {
+    console.log(oldIndex," ",newIndex)
+  };
   render(){
     return (
       <div>
         <AddContact />
         <h3 className="contact-list-heading"> Contacts</h3>
-        <ul className="contact-list">
-          {this.props.contacts.map(contact =>
-            <Contact
-              key={contact._id}
-              {...contact}
-            />
-          )}
+        <ul>
+          <SortableList items={this.props.contacts} onSortEnd={this.onSortEnd}/>
         </ul>
       </div>
     )
