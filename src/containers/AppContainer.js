0a7252ddbc54a90a1b6944c689da728663fd1d5c
Praveen Selvaraj <praveen.beatle@gmail.com>
2017-05-18 14:27:56 +0530

    app container fetch state

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchState } from 'react-router-server';
+import { AppActions } from '../actions';
 
-export default () => {
-  return <div>heallo world</div>;
+@fetchState(
+  () => ({}),
+  actions => ({
+    done: actions.done,
+  }),
+)
+
+class AppContainer extends React.PureComponent {
+
+  componentWillMount() {
+    const { loggedIn, userId, authToken,
+      userAgent } = this.props;
+    setAuthToken(authToken);
+    this.props.done({
+      app: { loggedIn, userId, authToken, userAgent },
+    });
+  }
+
+  render() {
+    return (
+      <div>React Starter Kit</div>
+    );
+  }
+
 }
+const mapStateToProps = state => ({
+  return {
+    
+  }
+});
+
+export default connect(mapStateToProps, {
+  ...AppActions,
+})(AppContainer);
